5580379ece0b27e1f8d52aa0c80984ee46427b6d
Manuel1977 <Manuel_fe1977@hotmail.com>
2022-01-16 12:16:38 +0100

    pagina ppal y estructura

---
+++ src/app/shared/box-drop/box-drop.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box-drop',
+  templateUrl: './box-drop.component.html',
+  styles: [
+  ]
+})
+export class BoxDropComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/box-zone/box-zone.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box-zone',
+  templateUrl: './box-zone.component.html',
+  styles: [
+  ]
+})
+export class BoxZoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
